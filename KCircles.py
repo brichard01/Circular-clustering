c88a53e6a1ff679ceb29bf5201c7edd7c325c366
brichard01 <baptisterichard.centrale@gmail.com>
2025-02-23 16:56:28 +0100

    Initial Commit

---
+++ KCircles.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class KCircles:
+     
+    def __init__(self, k_circles, n_features=2) -> None:
+        self.k_circles = k_circles
+        self.n_features = n_features
+        self.radius = None
+        self.histo = None
+
+    def init_paramters(self, X):
+        std = X.std(axis=0).reshape(self.n_features)
+        med = np.median(X, axis=0).reshape(self.n_features)
+        centers = []
+        for j in range(self.n_features):
+            s, m = std[j], med[j]    
+            centers.append(m + s - 2*s*np.random.random(self.k_circles))
+
+        self.centers = np.column_stack(centers)
+        self.radius = np.random.rand(self.k_circles) + 0.5
+     
+    def fit(self, X, iter=2000, init=True) -> None:
+        if init:
+            self.init_paramters(X)
+
+        if self.histo == None :
+            self.histo = [self.centers.copy()]
+
+        display = 1
+        if iter>5:
+            display = iter // 5
+
+        for i in range(iter): 
+            D = np.column_stack([np.sqrt(((X-self.centers[j])**2).sum(axis=1)) for j in range(self.k_circles)])
+            L = (D.copy() - np.array(self.radius).reshape(1, self.k_circles))**2
+            y = L.argmin(axis=1)
+            for j in range(self.k_circles):
+                Dj = D[y==j, j]
+                size = len(Dj)
+                grad = -(X[y==j]-self.centers[j])/Dj.reshape(size, 1)
+                grad = np.hstack((-2*np.ones((grad.shape[0], 1)), grad))
+                grad = grad * (Dj-self.radius[j]).reshape(size, 1) / size
+                grad = grad.sum(axis=0)
+                self.radius[j] -= 1/10**2 * grad[0]
+                self.centers[j] = self.centers[j] - 1/10**1 * grad[1:]
+            if i % display == 0:
+                for j in range(self.k_circles):
+                    #print(j, ':', self.centers[j], self.centers[j])
+                    pass
+            self.histo.append(self.centers.copy())
+
+    def multi_fit(self, X, iter, rep):
+        loss = 10**9
+        for i in range(rep):
+            self.fit(X, iter)
+            y_, loss_ = self.predict(X, loss=True)
+            if loss_<loss:
+                loss = loss_
+                centers = self.centers.copy()
+                radius = self.radius
+                histo = self.histo            
+            self.histo = None
+        self.centers = centers
+        self.radius = radius
+        self.histo = histo
+     
+    def predict(self, X, loss=False):
+        D = np.column_stack([np.sqrt(((X-self.centers[j])**2).sum(axis=1)) for j in range(self.k_circles)])
+        L = (D.copy() - np.array(self.radius).reshape(1, self.k_circles))**2
+        if loss:
+            return L.argmin(axis=1), L.min(axis=1).sum()
+        return L.argmin(axis=1)
+        
+    def fit_predict(self, X, iter):
+        self.fit(X, iter)
+        return self.predict(X)
